efce59e31cdd428e95b61d27e37d922a407426b5
Ian Matheson <iama2027@colorado.edu>
2019-11-12 15:03:46 -0700

    More playlist ids and merged together removing duplicates

--- classifier.py
+++ classifier.py
@@ -15,9 +15,7 @@ from math import pi
 
 
 #-------------Variables--------------
-# export SPOTIPY_CLIENT_ID='9d6f6b55c63b442c825236c74f4d8cc5'
-# export SPOTIPY_CLIENT_SECRET='2ee56909011b41e3bc7c25299b9b41cb'
-# export SPOTIPY_REDIRECT_URI='https://developer.spotify.com/dashboard/applications/9d6f6b55c63b442c825236c74f4d8cc5'
+#we could not make these global if that is better?????
 cid = "9d6f6b55c63b442c825236c74f4d8cc5"
 secret = "2ee56909011b41e3bc7c25299b9b41cb"
 redirect_uri = "https://developer.spotify.com/dashboard/applications/9d6f6b55c63b442c825236c74f4d8cc5"
@@ -35,9 +33,11 @@ rap7ID = "2SmIysAFFMz4cBPPaQ70v7" #https://open.spotify.com/playlist/2SmIysAFFMz
 rap8ID = "2mlGr2NYuJXRA7M6GYhmNZ" #https://open.spotify.com/playlist/2mlGr2NYuJXRA7M6GYhmNZ Hip-hop Lit
 rap9ID = "5iA8wivZWLZ9iCLrvj1PLV" #https://open.spotify.com/playlist/5iA8wivZWLZ9iCLrvj1PLV 90's rap
 
+rapIDs = [rap1ID, rap2ID, rap3ID, rap4ID, rap5ID, rap6ID, rap7ID, rap8ID, rap9ID]
+
 #ROCK
 rock1ID = "37i9dQZF1DWXRqgorJj26U" #https://open.spotify.com/playlist/37i9dQZF1DWXRqgorJj26U Rock classics
-rock2ID = "3Ho3iO0iJykgEQNbjB2sic" #https://open.spotify.com/playlist/3Ho3iO0iJykgEQNbjB2sic Rock classics 70's, 80's, 90's
+rock2ID = "37i9dQZF1DX1rVvRgjX59F" #https://open.spotify.com/playlist/37i9dQZF1DX1rVvRgjX59F 90's rock anthems
 rock3ID = "5RkXZzyPCKrovrl1XF92vo" #https://open.spotify.com/playlist/5RkXZzyPCKrovrl1XF92vo 70's Rock Classics
 rock4ID = "1NIX36ZFWEtgXSbSNghoue" #https://open.spotify.com/playlist/1NIX36ZFWEtgXSbSNghoue Hard Rock Classics
 rock5ID = "00VTH4862l4NRuq6F3goLA" #https://open.spotify.com/playlist/00VTH4862l4NRuq6F3goLA Rocks Classics
@@ -47,6 +47,9 @@ rock8ID = "4oIIueI3vnEI6RtGuOrwzr" #https://open.spotify.com/playlist/4oIIueI3vn
 rock9ID = "4282qXajfoO3scbvogeQCW" #https://open.spotify.com/playlist/4282qXajfoO3scbvogeQCW Classics (my playlist)
 rock10ID = "2q7gVPXAMNC73Mxqb0LJoC" #https://open.spotify.com/playlist/2q7gVPXAMNC73Mxqb0LJoC Exam 2
 rock11ID = "1MfTMvgM5bCsctoUtKwbqy" #https://open.spotify.com/playlist/1MfTMvgM5bCsctoUtKwbqy Exam 3 (all from rock class)
+rock12ID = "4lIywN6kXl9KPm3OQ8u8G7" #https://open.spotify.com/playlist/4lIywN6kXl9KPm3OQ8u8G7 Classic rock radio
+
+rockIDs = [rock1ID, rock2ID, rock3ID, rock4ID, rock5ID, rock6ID, rock7ID, rock8ID, rock9ID, rock10ID, rock11ID, rock12ID]
 
 #JAZZ
 jazz1ID = "0kpec6BHD5cVFLjYnOqJgL" #https://open.spotify.com/playlist/0kpec6BHD5cVFLjYnOqJgL Bar jazz classics
@@ -55,10 +58,17 @@ jazz3ID = "278sQOsiKoPwJqpvHfjqsV" #https://open.spotify.com/playlist/278sQOsiKo
 jazz4ID = "4KCzwOy4LtlAd0UwCTYOhs" #https://open.spotify.com/playlist/4KCzwOy4LtlAd0UwCTYOhs Jazz Classics
 jazz5ID = "5WorvtdlSDEQI8FFx2bB41" #https://open.spotify.com/playlist/5WorvtdlSDEQI8FFx2bB41 Jas (my playlist - check it out)
 jazz6ID = "6sU3nSuJzb63Md91cJXs7w" #https://open.spotify.com/playlist/6sU3nSuJzb63Md91cJXs7w Jass
+jazz7ID = "37i9dQZF1DX7UE7qrnkvsf" #https://open.spotify.com/playlist/37i9dQZF1DX7UE7qrnkvsf jazz for autumn
+jazz8ID = "6UHNDehnrX6zizwRaLHoZE" #https://open.spotify.com/playlist/6UHNDehnrX6zizwRaLHoZE big band and swing jazz
+jazz9ID = "05Hd48jdQIz3s8WRrvGnzf" #https://open.spotify.com/playlist/05Hd48jdQIz3s8WRrvGnzf Jazz Playlist
 
-#
+jazzIDs = [jazz1ID, jazz2ID, jazz3ID, jazz4ID, jazz5ID, jazz6ID, jazz7ID, jazz8ID, jazz9ID]
 
+#CLASSICAL
 
+#COUNTRY
+
+#EDM
 
 
 #----------AUTHORIZATION--------------
@@ -72,22 +82,51 @@ else:
     print("Can't get token for", username)
 
 #-----------INITIALIZATION------------
-#need different song lists in order to train my classifier
-longestPlay = sp.user_playlist(username, longestPlayID)
-longestTracks = longestPlay["tracks"]
-longestPlaySongs = longestTracks["items"]
+def mergePlaylists(ids):
+    allSongIDs = []
+    for id in ids:
+        playlist = sp.user_playlist(username, id)
+        playlistTracks = (playlist["tracks"])
+        playlistSongs = playlistTracks["items"]
+        for song in playlistSongs:
+            if song['track']['id'] in allSongIDs:
+                continue
+            else:
+                allSongIDs.append(song['track']['id'])
 
-for song in longestPlaySongs:
-    features = sp.audio_features(song['track']['id'])
-    track = sp.track(song['track']['id'])
-    album = sp.album(track['album']['id'])
-    print(album['genres'])
+    return allSongIDs
+
+def getFeatures(songIDs):
+    #SOMEHOW DEAL WITH FEATURE EXTRACTION AND AVERAGING???
+    for id in songIDs:
+        features = sp.audio_features(id)
+        print(features)
+
+
+
+#need different song lists in order to train classifier
+rapSongIDs = mergePlaylists(rapIDs)
+print("Rap length: ", len(rapSongIDs))
+rockSongIDs = mergePlaylists(rockIDs)
+print("Rock length: ", len(rockSongIDs))
+jazzSongIDs = mergePlaylists(jazzIDs)
+print("Jazz length: ", len(jazzSongIDs))
+
+
+# longestTracks = longestPlay["tracks"]
+# longestPlaySongs = longestTracks["items"]
+
+# for song in longestPlaySongs:
+#     features = sp.audio_features(song['track']['id'])
+#     track = sp.track(song['track']['id'])
+#     album = sp.album(track['album']['id'])
+#     print(album['genres'])
 
 
 #-----------DATA CLEANING-------------
-features = sp.audio_features(longestPlaySongs[0]['track']['id'])
-track = sp.track(longestPlaySongs[0]['track']['id'])
-album = sp.album(track['album']['id'])
+# features = sp.audio_features(longestPlaySongs[0]['track']['id'])
+# track = sp.track(longestPlaySongs[0]['track']['id'])
+# album = sp.album(track['album']['id'])
 # print(album['genres'])
 # print(track['album'])
 # release_date = track['album']['release_date']
