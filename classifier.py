66de83206703809c1d3f85144cd56b9efad043d3
Ian Matheson <iama2027@colorado.edu>
2019-11-13 00:17:48 -0700

    all playlists found and each genre has between 2000-2200 songs

--- classifier.py
+++ classifier.py
@@ -49,32 +49,67 @@ rock9ID = "4282qXajfoO3scbvogeQCW" #https://open.spotify.com/playlist/4282qXajfo
 rock10ID = "2q7gVPXAMNC73Mxqb0LJoC" #https://open.spotify.com/playlist/2q7gVPXAMNC73Mxqb0LJoC Exam 2
 rock11ID = "1MfTMvgM5bCsctoUtKwbqy" #https://open.spotify.com/playlist/1MfTMvgM5bCsctoUtKwbqy Exam 3 (all from rock class)
 rock12ID = "4lIywN6kXl9KPm3OQ8u8G7" #https://open.spotify.com/playlist/4lIywN6kXl9KPm3OQ8u8G7 Classic rock radio
+rock13ID = "00o5lm5x6yP9rIJIx6qjrJ" #https://open.spotify.com/playlist/00o5lm5x6yP9rIJIx6qjrJ More alt rock and current
+rock14ID = "01BCSQLJo2krgnu0OZhG0L" #https://open.spotify.com/playlist/01BCSQLJo2krgnu0OZhG0L Rock n roll classics
 
-rockIDs = [rock1ID, rock2ID, rock3ID, rock4ID, rock5ID, rock6ID, rock7ID, rock8ID, rock9ID, rock10ID, rock11ID, rock12ID]
+rockIDs = [rock1ID, rock2ID, rock3ID, rock4ID, rock5ID, rock6ID, rock7ID, rock8ID, rock9ID, rock10ID, rock11ID, rock12ID, rock13ID, rock14ID]
 
 #JAZZ
-jazz1ID = "0kpec6BHD5cVFLjYnOqJgL" #https://open.spotify.com/playlist/0kpec6BHD5cVFLjYnOqJgL Bar jazz classics
+# jazz1ID = "0kpec6BHD5cVFLjYnOqJgL" #https://open.spotify.com/playlist/0kpec6BHD5cVFLjYnOqJgL Bar jazz classics
 jazz2ID = "5hZtfYA9YY8kRq5bPPOtQP" #https://open.spotify.com/playlist/5hZtfYA9YY8kRq5bPPOtQP Dixieland classics
-jazz3ID = "278sQOsiKoPwJqpvHfjqsV" #https://open.spotify.com/playlist/278sQOsiKoPwJqpvHfjqsV Jazz for dining
+# jazz3ID = "278sQOsiKoPwJqpvHfjqsV" #https://open.spotify.com/playlist/278sQOsiKoPwJqpvHfjqsV Jazz for dining
 jazz4ID = "4KCzwOy4LtlAd0UwCTYOhs" #https://open.spotify.com/playlist/4KCzwOy4LtlAd0UwCTYOhs Jazz Classics
 jazz5ID = "5WorvtdlSDEQI8FFx2bB41" #https://open.spotify.com/playlist/5WorvtdlSDEQI8FFx2bB41 Jas (my playlist - check it out)
-jazz6ID = "6sU3nSuJzb63Md91cJXs7w" #https://open.spotify.com/playlist/6sU3nSuJzb63Md91cJXs7w Jass
-jazz7ID = "37i9dQZF1DX7UE7qrnkvsf" #https://open.spotify.com/playlist/37i9dQZF1DX7UE7qrnkvsf jazz for autumn
+# jazz6ID = "6sU3nSuJzb63Md91cJXs7w" #https://open.spotify.com/playlist/6sU3nSuJzb63Md91cJXs7w Jass
+# jazz7ID = "37i9dQZF1DX7UE7qrnkvsf" #https://open.spotify.com/playlist/37i9dQZF1DX7UE7qrnkvsf jazz for autumn
 jazz8ID = "6UHNDehnrX6zizwRaLHoZE" #https://open.spotify.com/playlist/6UHNDehnrX6zizwRaLHoZE big band and swing jazz
 jazz9ID = "05Hd48jdQIz3s8WRrvGnzf" #https://open.spotify.com/playlist/05Hd48jdQIz3s8WRrvGnzf Jazz Playlist
+# jazz10ID = "478wi4LZyjCS1Jo6y26xnI" #https://open.spotify.com/playlist/478wi4LZyjCS1Jo6y26xnI
+jazz11ID = "7A36aaHgeJ3TXlCsVr86CX" #https://open.spotify.com/playlist/7A36aaHgeJ3TXlCsVr86CX GIANT GUY - may delete and redo
 
-jazzIDs = [jazz1ID, jazz2ID, jazz3ID, jazz4ID, jazz5ID, jazz6ID, jazz7ID, jazz8ID, jazz9ID]
+
+jazzIDs = [jazz2ID, jazz4ID, jazz5ID, jazz8ID, jazz9ID, jazz11ID]
 
 #CLASSICAL
+classical1ID = "5tXCRZAUKp2uqtmJZNkQxY" #https://open.spotify.com/playlist/5tXCRZAUKp2uqtmJZNkQxY classical
+classical2ID = "3fNcrDFpar3QMWAc5fji9G" #https://open.spotify.com/playlist/3fNcrDFpar3QMWAc5fji9G "     "
+classical3ID = "0UNHDTybzvJrHmFASPnVgj" #https://open.spotify.com/playlist/0UNHDTybzvJrHmFASPnVgj "     "
+classical4ID = "7vbMPSU3IVLBQSDgucTUMP" #https://open.spotify.com/playlist/7vbMPSU3IVLBQSDgucTUMP "     "
+classical5ID = "37i9dQZF1DWWEJlAGA9gs0" #https://open.spotify.com/playlist/37i9dQZF1DWWEJlAGA9gs0 "     "
+classical6ID = "1pcKs0Rig9FiVPrj87Iffv" #https://open.spotify.com/playlist/1pcKs0Rig9FiVPrj87Iffv classical essentials
+classical7ID = "2Ea3nBAL68c1JQqMC4PMar" #https://open.spotify.com/playlist/2Ea3nBAL68c1JQqMC4PMar classical essentials
+classical8ID = "1lS6K85kThX7lkPjJ23daR" #https://open.spotify.com/playlist/1lS6K85kThX7lkPjJ23daR my classical essentials
+classical9ID = "1aeZv1CYwR39C6lf2mK1Bj" #https://open.spotify.com/playlist/1aeZv1CYwR39C6lf2mK1Bj
+classicalIDs = [classical1ID, classical2ID, classical3ID, classical4ID, classical5ID, classical6ID, classical7ID, classical8ID, classical9ID]
 
 #COUNTRY
-country1ID = "2rlCIi7oKEyKTN42BUdMSB" #https://open.spotify.com/playlist/2rlCIi7oKEyKTN42BUdMSB maura Country
+country1ID = '4v4zEO7FPn9lKt7pLHao5X' # The only country playlist that matters https://open.spotify.com/playlist/4v4zEO7FPn9lKt7pLHao5X 
+country2ID = "2rlCIi7oKEyKTN42BUdMSB" #https://open.spotify.com/playlist/2rlCIi7oKEyKTN42BUdMSB maura Country
+country3ID = "5SvjmWVcoJkac9BKud2M6W" #https://open.spotify.com/playlist/5SvjmWVcoJkac9BKud2M6W country
+country4ID = "5AOSddZQa4C9bt4E7luY1L" #https://open.spotify.com/playlist/5AOSddZQa4C9bt4E7luY1L Country
+country5ID = "48OHCGqtPpbOZLFpN35Xsp" #https://open.spotify.com/playlist/48OHCGqtPpbOZLFpN35Xsp Top country 2013-2019
+country6ID = "6o78UWoUeQLks9G785NLsf" #https://open.spotify.com/playlist/6o78UWoUeQLks9G785NLsf
+country7ID = "13HYHjm9Tv5QWJIFSQlv7h" #https://open.spotify.com/playlist/13HYHjm9Tv5QWJIFSQlv7h Country Classics
+country8ID = "7fOzCZJTRCmvZ0G3Zy4AVW" #https://open.spotify.com/playlist/7fOzCZJTRCmvZ0G3Zy4AVW Country new and old
+country9ID = "0t6GshCAD6ym2ZWz7rjzoZ" #https://open.spotify.com/playlist/0t6GshCAD6ym2ZWz7rjzoZ country playlist
 
-countryIDs = [country1ID]
+countryIDs = [country1ID, country2ID, country3ID, country4ID, country5ID, country6ID, country7ID, country8ID, country9ID]
+
 #EDM
 edm1ID = "0tEj701zR0xgOXhXucX57B" #https://open.spotify.com/playlist/0tEj701zR0xgOXhXucX57B maura edm (abg)
+edm2ID = "4xXt0NNILAMwXsXjkdIaAK" #https://open.spotify.com/playlist/4xXt0NNILAMwXsXjkdIaAK mike edm
+edm3ID = "4ykwAEFulaKPDQpflIr0k7" #https://open.spotify.com/playlist/4ykwAEFulaKPDQpflIr0k7 electric vibes?
+edm4ID = "5mhb5QRMXgufiqEuHL74gi" #https://open.spotify.com/playlist/5mhb5QRMXgufiqEuHL74gi
+edm5ID = "5P1PcTioNlUzkW0bdr5FSg" #https://open.spotify.com/playlist/5P1PcTioNlUzkW0bdr5FSg edm classics
+edm6ID = "0rA7eold66Wk05o4irdrqq" #https://open.spotify.com/playlist/0rA7eold66Wk05o4irdrqq chill edm
+edm7ID = "3Di88mvYplBtkDBIzGLiiM" #https://open.spotify.com/playlist/3Di88mvYplBtkDBIzGLiiM edm classics 2019
+edm8ID = "2dL2ewiigZs9LuJHwBH2J0" #https://open.spotify.com/playlist/2dL2ewiigZs9LuJHwBH2J0
+edm9ID = "2R3Fh6q5rlpMbRU8oeRT1p" #https://open.spotify.com/playlist/2R3Fh6q5rlpMbRU8oeRT1p Insane (but more chill edm)
+edm10ID = "0sJLDKZxFWlcQFIIT45iQE" #https://open.spotify.com/playlist/0sJLDKZxFWlcQFIIT45iQE edm music
+edm11ID = "2W32HNDe5OgNsPvciLm1Ix" #https://open.spotify.com/playlist/2W32HNDe5OgNsPvciLm1Ix best edm music of all time
+edm12ID = "5Yn8cufVoLX4QubVGUOj9n" #https://open.spotify.com/playlist/5Yn8cufVoLX4QubVGUOj9n house music
 
-edmIDs = [edm1ID]
+edmIDs = [edm1ID, edm2ID, edm3ID, edm4ID, edm5ID, edm6ID, edm7ID, edm8ID, edm9ID, edm10ID, edm11ID, edm12ID]
 
 #----------AUTHORIZATION--------------
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
@@ -86,7 +121,7 @@ if token:
 else:
     print("Can't get token for", username)
 
-#-----------INITIALIZATION------------
+#-----------DATA HANDLING------------
 def mergePlaylists(ids):
     allSongIDs = []
     for id in ids:
@@ -111,19 +146,19 @@ def getFeatures(songIDs):
         features = sp.audio_features(id)
         print(features)
 
-#need different song lists in order to train classifier
+#PRINTING OUT LENTGTH OF ALL GENRES MERGED PLAYLISTS WITHOUT DUPLICATES
 rapSongIDs = mergePlaylists(rapIDs)
 print("Rap length: ", len(rapSongIDs))
 rockSongIDs = mergePlaylists(rockIDs)
 print("Rock length: ", len(rockSongIDs))
 jazzSongIDs = mergePlaylists(jazzIDs)
 print("Jazz length: ", len(jazzSongIDs))
+classicalSongIDs = mergePlaylists(classicalIDs)
+print("Classical length: ", len(classicalSongIDs))
 countrySongIDs = mergePlaylists(countryIDs)
 print("Country length: ", len(countrySongIDs))
 edmSongIDs = mergePlaylists(edmIDs)
 print("EDM length: ", len(edmSongIDs))
-
-print(len(get_playlist_tracks(rap10ID)))
 
 # longestTracks = longestPlay["tracks"]
 # longestPlaySongs = longestTracks["items"]
